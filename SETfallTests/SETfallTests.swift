5c003377b0d77a69cffb99b79d9c2bdfeb751a10
Student <student@students-macbook-pro.local>
2016-10-24 21:33:49 -0400

    Initial Commit

---
+++ SETfallTests/SETfallTests.swift
@@ -0,0 +1,36 @@
+//
+//  SETfallTests.swift
+//  SETfallTests
+//
+//  Created by Student on 2016-10-24.
+//  Copyright © 2016 WesNet. All rights reserved.
+//
+
+import XCTest
+@testable import SETfall
+
+class SETfallTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
